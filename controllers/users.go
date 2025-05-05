b41c5ecfa8adf88fb59ae834a1c4e917d4c29726
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2025-05-05 19:09:07 -0300

    Added URL query params to the sign up page to prefill the email

--- controllers/users.go
+++ controllers/users.go
@@ -12,7 +12,11 @@ type Usuarios struct {
 }
 
 func (u Usuarios) New(w http.ResponseWriter, r *http.Request) {
-	u.Templates.New.Execute(w, nil)
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.New.Execute(w, data)
 
 }
 
