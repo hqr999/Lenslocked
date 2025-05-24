40e7352ff7f5510678198580e13f880a45744386
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2025-05-24 19:07:20 -0300

    Created the function Autenticar that check if the user typed the correct password on the sigin

--- models/user.go
+++ models/user.go
@@ -30,9 +30,30 @@ func (u *UserService) Criar(email, password string) (*User, error) {
 	col := u.Banco_Dados.QueryRow(`
 			INSERT INTO users (email,password_hash)
 			VALUES ($1, $2) RETURNING id`, email, passwordHash)
-		erro = col.Scan(&usuario.ID)
-		if erro != nil {
-			return nil,fmt.Errorf("Usuário criado: %w",erro)
+	erro = col.Scan(&usuario.ID)
+	if erro != nil {
+		return nil, fmt.Errorf("Usuário criado: %w", erro)
 	}
-	return &usuario,nil
+	return &usuario, nil
+}
+
+func (u UserService) Autenticar(email, password string) (*User, error) {
+	email = strings.ToLower(email)
+	usuario := User{Email: email}
+
+	coluna := u.Banco_Dados.QueryRow(`
+			SELECT id, password_hash FROM users WHERE email=$1
+		`, email)
+	err := coluna.Scan(&usuario.ID, &usuario.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("Autenticação: %v", err)
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(usuario.PasswordHash), []byte(password))
+	if err != nil {
+		return nil, fmt.Errorf("Autenticação: %v", err)
+	}
+	fmt.Println("Senha está correta")
+
+	return &usuario, nil
 }
