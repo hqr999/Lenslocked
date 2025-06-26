36d02560616e338d018bac82e941f9acf33efb7a
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2025-06-26 09:41:09 -0300

    Created the methods for interacting with our session service

--- models/session.go
+++ models/session.go
@@ -1,7 +1,30 @@
-package models 
+package models
+
+import "database/sql"
 
 type Session struct {
-		ID int 
-		UserID int 
-		TokenHash string
+	ID     int
+	UserID int
+	// Toke é somente setado quando cria-se uma nova sessão
+	//quando buscamos por uma sessão, esse campo será deixado vazio
+	//,uma vez que salvamos um token de sessão em nosso BD
+	//e nós não podemos revertê-lo para um token em texto claro.
+	Token     string
+	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userId int) (*Session, error) {
+	//A FAZER Cria um token de sessão
+	//A FAZER Implementar SessionService.Create
+	return nil, nil
+
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	//A FAZER Implementar SessionService.user
+	return nil, nil 
 }
