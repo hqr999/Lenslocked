42da4ba774632ef26e2b22262365c0696215eb02
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2025-08-02 17:38:22 -0300

    Created our email service that deals with the email sending

--- models/email.go
+++ models/email.go
@@ -1,27 +1,39 @@
 package models
 
-import "github.com/go-mail/mail/v2"
+import (
+	"fmt"
+
+	"github.com/go-mail/mail/v2"
+)
 
 const (
 	RemetentePadrao = "suporte@lenslocked.com"
 )
 
-type SMTPConfig struct {
-	Host string
-	Port int 
-	Username string 
-	Password string 
+type Email struct {
+	De      string
+	Para    string
+	Assunto string
+	Texto   string
+	HTML    string
 }
 
-func NovoServicoEmail(conf SMTPConfig) (*EmailServicos) {
-		es := EmailServicos{
-		//Precisamos preencher os campo do dialer 
-		dialer: mail.NewDialer(conf.Host,conf.Port,conf.Username,conf.Password),
+type SMTPConfig struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+}
+
+func NovoServicoEmail(conf SMTPConfig) *EmailServico {
+	es := EmailServico{
+		//Precisamos preencher os campo do dialer
+		dialer: mail.NewDialer(conf.Host, conf.Port, conf.Username, conf.Password),
 	}
 	return &es
 }
 
-type EmailServicos struct {
+type EmailServico struct {
 	//O RemetentePadrao é usado como o remetente padrao
 	//quando um não é fornecido para um email.
 	//Isso também é usado em funções onde o email é
@@ -32,4 +44,37 @@ type EmailServicos struct {
 	dialer *mail.Dialer
 }
 
+func (es *EmailServico) Mandar(email Email) error {
+	mensagem := mail.NewMessage()
+	mensagem.SetHeader("To", email.De)
+	es.setFrom(mensagem,email)
+	mensagem.SetHeader("Subject", email.Assunto)
+	switch {
+	case email.Texto != "" && email.HTML != "":
+		mensagem.SetBody("text/plain", email.Texto)
+		mensagem.AddAlternative("text/html", email.HTML)
+	case email.Texto != "":
+		mensagem.SetBody("text/plain", email.Texto)
+	case email.HTML != "":
+		mensagem.AddAlternative("text/html", email.HTML)
+	}
 
+	err := es.dialer.DialAndSend(mensagem)
+	if err != nil {
+		return fmt.Errorf("send: %w", err)
+	}
+	return nil
+}
+
+func (es *EmailServico) setFrom(msg *mail.Message,email Email) {
+		var de string 
+		switch  {
+		case email.De != "":
+			de = email.De
+		case es.RemetentePadrao != "":
+			de = es.RemetentePadrao
+	default:
+			de = RemetentePadrao
+		}
+	msg.SetHeader("from",de)
+}
