aa61cfa36c43e00263ff193b77e8dec1d870f9b3
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2025-09-15 00:23:34 -0300

    Added a function that allows querying for galleries by userID

--- models/gallery.go
+++ models/gallery.go
@@ -30,19 +30,47 @@ func (gs GalleryService) Create(title string, userID int) (*Gallery, error) {
 }
 
 func (gs GalleryService) ByID(id int) (*Gallery, error) {
-	//A FAZER:Validação para o ID 
+	//A FAZER:Validação para o ID
 	galeria := Gallery{ID: id}
 
 	linha := gs.BD.QueryRow(`
 			SELECT title, user_id
 			FROM galleries
 			WHERE id = $1;`, galeria.ID)
-	err := linha.Scan(&galeria.Title,&galeria.UserID)
+	err := linha.Scan(&galeria.Title, &galeria.UserID)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows){
-				return nil, ErrNotFound 
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound
 		}
-		return nil,fmt.Errorf("query gallery by id: %w",err)
+		return nil, fmt.Errorf("query gallery by id: %w", err)
 	}
-	return &galeria,nil 
+	return &galeria, nil
+}
+
+func (gs *GalleryService) ByUserID(userID int) ([]Gallery, error) {
+	linhas, erros := gs.BD.Query(`
+				SELECT * FROM galleries
+				WHERE user_id = $1;`, userID)
+
+	if erros != nil {
+		return nil, fmt.Errorf("query galleries by user id: %w", erros)
+
+	}
+	var galerias []Gallery
+	for linhas.Next() {
+		gal := Gallery{
+			UserID: userID,
+		}
+		erros = linhas.Scan(&gal.ID, &gal.Title)
+		if erros != nil {
+			return nil, fmt.Errorf("query galleries by user id:%w", erros)
+
+		}
+		galerias = append(galerias, gal)
+	}
+
+	if linhas.Err() != nil {
+		return nil, fmt.Errorf("query galleries by user id: %w", erros)
+	}
+	return galerias, nil
 }
