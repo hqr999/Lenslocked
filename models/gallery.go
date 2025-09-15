30a6455eecbf6eea6b5ea4c6e17614d3d84ef814
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2025-09-14 21:02:39 -0300

    Developed our basic Create function on models/gallery.go that will create a gallery on our database

--- models/gallery.go
+++ models/gallery.go
@@ -1,7 +1,29 @@
-package models 
+package models
 
-type Gallery struct{
-	ID int 
-	UserID int 
-	Title string 
+import (
+	"database/sql"
+	"fmt"
+)
+
+type Gallery struct {
+	ID     int
+	UserID int
+	Title  string
+}
+
+type GalleryService struct {
+	BD *sql.DB
+}
+
+func (gs GalleryService) Create(title string, userID int) (*Gallery, error) {
+	galeria := Gallery{Title: title, UserID: userID}
+	linha := gs.BD.QueryRow(`
+				INSERT INTO galleries (title,user_id)
+				VALUES ($1,$2) RETURNING id;`, galeria.Title, galeria.UserID)
+	error := linha.Scan(&galeria.ID)
+	if error != nil {
+		return nil, fmt.Errorf("create gallery: %w", error)
+	}
+
+	return &galeria, nil
 }
